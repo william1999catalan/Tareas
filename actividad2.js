bd36923987fb53b86d4af4d4c03830576359c3dd
William Catalan <william.catalan@galileo.edu>
2020-02-03 23:15:12 -0600

    Se agrego la Actividad 2

---
+++ actividad2.js
@@ -0,0 +1,20 @@
+var mes = 10
+
+if((mes==12) || (mes==1) || (mes==2))
+{
+  document.write("Es Invierno"+"<br>")
+}
+else if ((mes>=3) && (mes<=5))
+{
+  document.write("Es Primavera"+"<br>")
+}
+else if((mes>=6) && (mes<=8))
+{
+  document.write("Es Verano"+"<br>")
+}
+else if((mes>=9) && (mes<=11))
+{
+  document.write("Es Otoño"+"<br>")
+}
+
+document.write("20002284, William Jesualdo Catalan Pineda")
